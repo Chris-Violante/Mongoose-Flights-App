9f1a29b16b5fd8d1b69c5efd6df8679ec2de199b
Christopher Violante <christopherviolante@Christophers-MacBook-Pro.local>
2020-05-22 15:35:54 -0600

    mongoose link and model ready

---
+++ models/flights.js
@@ -0,0 +1,28 @@
+const mongoose = vrequire('mongoose');
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United']
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN'
+    }, 
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999
+    },
+    departs: {
+        type: Date,
+        default: function() {
+            const dt = new Date();
+            return dt.setFullYear(dt.getFullYear() + 1);
+        }
+    } 
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
